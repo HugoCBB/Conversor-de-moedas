216a649c4d508c447b9177f1264c60180502a39c
Hugo <hugocarlosb.071@gmail.com>
2025-01-11 23:46:16 -0300

    feat: Adicionado consumo da API funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// import Api from "./components/api"
-import Input from "./components/input"
+
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Input from "./components/Input"
+
 
 
 function App() {
+  const [moedas, setMoedas] = useState([]); // Inicialize como um array vazio
 
+  async function getMoedas(key) {
+      try {
+          const response = await Axios.get(`https://v6.exchangerate-api.com/v6/${key}/latest/USD`);
+          setMoedas(Object.keys(response.data.conversion_rates)); // Define as chaves como array
+      } catch (error) {
+          console.error("Erro na requisição: ", error);
+      }
+  }
+
+  useEffect(() => {
+      getMoedas("83fc1f2415e19e4528dd8644");
+  }, []);
 
   return (
     <>
-      <Input/>
+      <Input 
+      moeda={moedas}
+      />
     </>
   )
 }
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './Input.css'
 
-const Input = () => {
+const Input = ({moeda}) => {
+
+    const [isFocus, setIsFocus] = useState(false);
+
+
     return (
         <main className='input'>
             <section className='input-title'>
                 <h1>Conversor de moedas</h1>
             </section>
             <div className='input-fields'>
-                <input placeholder='USD - DOLAR' />
+                            
+                <div className='input-group'>
+                    <input
+                        placeholder='USD - DOLAR'
+                        onFocus={() => setIsFocus(true)}
+                    />
+                    {isFocus && (
+                        <div className='dropdown'>
+                            {moeda.map((moedas) => (
+                                <div key={moedas}>{moedas}</div>
+                            ))}
+                        </div>
+                    )}
+                </div>
                 <input placeholder='BRL - REAL ' />
             </div>
             <button>Converter</button>
