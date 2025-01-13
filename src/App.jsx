9f7e61de5222897f3236bcb66008312507f994f8
Hugo <hugocarlosb.071@gmail.com>
2025-01-12 22:43:09 -0300

    refactor(app): Adicionado consumo de de duas APIS simuntaneas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 
 import { useEffect, useState } from "react";
-import Axios from "axios";
 import Input from "./components/Input"
+import axios from "axios";
 
 
 
 function App() {
-  const [moedas, setMoedas] = useState([]); // Inicialize como um array vazio
+  const key = "83fc1f2415e19e4528dd8644"
+  const [moedas, setMoedas] = useState([]);
+  const [moedasConvertidas,setMoedasConvertidas] = useState([]);
 
-  async function getMoedas(key) {
-      try {
-          const response = await Axios.get(`https://v6.exchangerate-api.com/v6/${key}/latest/USD`);
-          setMoedas(Object.keys(response.data.conversion_rates)); // Define as chaves como array
-      } catch (error) {
-          console.error("Erro na requisição: ", error);
-      }
-  }
 
   useEffect(() => {
-      getMoedas("83fc1f2415e19e4528dd8644");
+    const getMoedas = async () => {
+    try {
+      const [response1, response2] = await Promise.all([
+        axios.get(`https://v6.exchangerate-api.com/v6/${key}/latest/USD`),
+        axios.get(`https://v6.exchangerate-api.com/v6/${key}/pair/EUR/BRL/1`)
+      ]);
+
+      setMoedas(Object.keys((response1.data.conversion_rates)))
+      setMoedasConvertidas(response2.data)
+
+    } catch (error) {
+      console.error("Erro na requisição: ", error);
+      
+    }}
+    getMoedas()
   }, []);
 
   return (
